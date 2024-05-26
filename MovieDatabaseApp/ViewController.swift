39aca8180de957f4c242c391f192aea0e9656957
bathalasupraja <bathalasupraja2@gmail.com>
2024-05-26 09:58:56 +0530

    Initial Commit

---
+++ MovieDatabaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDatabaseApp
+//
+//  Created by Supraja on 26/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
